429a86a83c1ff453280ae00832f7460120d062a8
Anderson R Damasceno <anderdam@gmail.com>
2023-12-20 10:33:56 -0300

    bug: adjusted bug at the end of questions and links added

--- aws_trainning.py
+++ aws_trainning.py
@@ -1,8 +1,6 @@
 import json
 import random
 
-# import rich
-
 hits, mistakes = [], []
 
 
@@ -19,75 +17,69 @@ def logo():
 
 
 def read_json() -> dict:
-    json_file = "questions.json"
+    json_file = "/home/anderdam/DataEngineering/scripts/aws_trainning/questions.json"
     with open(json_file) as js:
         return json.load(js)
 
 
-def initial_questions() -> dict:
-    return read_json()
-
-
-def reduced_questions(question_to_remove) -> dict:
-    new_questions = read_json()
-    del new_questions[question_to_remove]
-    return new_questions
-
-
-def get_question(questions_to_choose_from) -> tuple:
-    questions_list = questions_to_choose_from
-    limit = len(questions_list)
-    random_number = random.randint(1, limit)
-    question_number = f"Question {random_number}"
-    question = questions_list[question_number][0]
-    right_answer = questions_list[question_number][1]
-    check = questions_list[question_number][2]  # Use the information or remove access
-    return question_number, question, right_answer, check
+def shuffled_questions():
+    questions = list(
+        read_json().items()
+    )  # Convert dict to list of (question_number, question_data) tuples
+    random.shuffle(questions)  # Shuffle the list in-place
+    return questions
 
 
 def score(hit, mistake):
     return f"""
     #########################################
-    ### Score: Rights - {len(hit)} X Mistakes - {len(mistake)} ###
+    ###              Score                ### 
+    ###         Hits {len(hit)} X {len(mistake)} Mistakes       ###
     #########################################
     """
 
 
-def start(question_tuple, limit):
-    question_number, display_question, right_answer, check = question_tuple
+def start(question_list):
+    limit = len(question_list) - 1
+    for question_item in question_list:
+        question_number = question_item[0]
+        question = question_item[1][0]
+        right_answer = question_item[1][1]
+        link = question_item[1][2]
 
-    limit = limit
+        print(question_number, f"(Remainning: {limit})")
+        print(question)
+        print("")
 
-    print(question_number, f"(Remainning: {limit})")
-    print(display_question)
-    print("")
+        user_answer = input("Enter a option: ")
+        print("\n")
+        if user_answer.upper() == right_answer.upper():
+            hits.append(question_number)
+            print("*** CORRECT ***")
+        else:
+            mistakes.append(question_number)
+            print("!!! WRONG !!!")
+            print(f"Correct answer was: {right_answer}")
+            print(link)
 
-    user_answer = input("Enter a option: ")
-    print("\n")
-    if user_answer.upper() == right_answer.upper():
-        hits.append(question_number)
-        print("*** CORRECT ***")
-    else:
-        mistakes.append(question_number)
-        print("!!! WRONG !!!")
-        print(f"Correct answer was: {right_answer}")
-        print(check)
+        print(score(hits, mistakes))
 
-    print(score(hits, mistakes))
-
-    next_question = input("Continue to next question ([Y]es/[N]o)? \n").lower()
-    if next_question == "yes" or next_question == "y":
-        limit -= 1
-        start(get_question(reduced_questions(question_number)), limit=limit)
+        next_question = input("Continue to next question ([Y]es/[N]o)? ").lower()
+        print("")
+        if next_question == "yes" or next_question == "y":
+            limit -= 1
+            continue
         # TODO Maybe add a stop condition based on limit or dictionary size
-    else:
-        print("### Fim de jogo ###\n")
-        print(f"Resultado final:\n{score(hits, mistakes)}")
+        else:
+            break
+
+    print("### Fim de jogo ###\n")
+    print(f"Resultado final:\n{score(hits, mistakes)}")
 
 
 if __name__ == "__main__":
     logo()
     try:
-        start(get_question(initial_questions()), limit=len(initial_questions()))
+        start(shuffled_questions())
     except KeyboardInterrupt:
         print("\nThanks for your time and good luck in your certification :) ")
